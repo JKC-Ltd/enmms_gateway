d092997efaf3ecea45a591900c3dddb60db09ed0
C H A R L E S <51162579+charlitovicente@users.noreply.github.com>
2025-04-29 22:46:15 +0800

    Update gateway_config.py

--- gateway_config.py
+++ gateway_config.py
@@ -6,8 +6,8 @@ from datetime import datetime
 import sys
 
 # DECLARING ID's
-gateway_id      = 2
-gateway_code    = "GAT-02"
+gateway_id      = 6
+gateway_code    = "GAT-06"
 
 
 def get_metter_ids():
