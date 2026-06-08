2bdf76f836720f76d704e5aa8145aa5f16a2936b
C H A R L E S <charleslei2017@gmail.com>
2026-06-08 18:14:58 +0800

    Update index.py

--- index.py
+++ index.py
@@ -64,12 +64,12 @@ for meter_result in meter_results:
         if client.connect():
             try:
 
-                # if model_id == 1: # model_id = 1 = Schneider
-                #     response = client.read_holding_registers(address=int(register_address), count=2, slave=slave_address)
-                # else:
-
-                response = client.read_input_registers(address=int(
-                    register_address), count=2, slave=slave_address)
+                if model_id == 1:  # model_id = 1 = Schneider
+                    response = client.read_holding_registers(address=int(
+                        register_address), count=2, slave=slave_address)
+                else:
+                    response = client.read_input_registers(address=int(
+                        register_address), count=2, slave=slave_address)
 
                 # response = client.read_input_registers(address=0, count=2, slave=6)
                 if not response.isError():
